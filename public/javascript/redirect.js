b03a29a4ea5a7345c1bd5d73076146d68d3a45cd
ad_osuka_t <osuka_t@a-drop.com>
2018-11-05 17:59:56 +0900

    Cloud Firestoreの使い方

--- public/javascript/redirect.js
+++ public/javascript/redirect.js
@@ -1,7 +1,11 @@
 // setTimeout("window.location.replace('https://www.google.co.jp/')",0);
 var database = firebase.database();
 var redirectURL = 'https://www.google.co.jp';
-var read = database.ref('shortens/s000001').child('source_url');
+// var read = database.ref('shortens/s000001').child('source_url');
+var params = (new URL(document.location)).searchParams;
+var value = params.get('sid');
+var read = database.ref('shortens/' + value).child('source_url');
+
 read.once('value', function(snapshot){
   if(snapshot != null){
       redirectURL = snapshot.val();
